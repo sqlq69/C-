f96bc8290d5427a5584c8f5facba23e9eb753653
unknown <q69_wow@list.ru>
2022-10-07 19:16:03 +0300

    самая сложная для меня оказалась вторая)

---
+++ task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿            Console.WriteLine("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int max = a;
+              if (b > a)
+              max = b;
+              if (c > max)
+              max = c;
+             Console.Write(max);
