9218444f6ef023ad69f8d1549bb128c671c96c0b
unknown <q69_wow@list.ru>
2022-10-07 18:07:39 +0300

    добавил 6 задачу

---
+++ task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿  int a;                             
+            Console.Write("введите число: ");
+            a= Console.Read();
+ 
+            if (a % 2==1)
+            {
+                Console.WriteLine("число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+ 
